18ee59e3939ec09fa752749cd1feb2f3dfce7187
Kevin Dave <kevindavee1505@gmail.com>
2022-04-11 19:24:59 +0700

    setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const { createCanvas, loadImage } = require("canvas");
+const canvas = createCanvas(1000, 1000);
+const ctx = canvas.getContext("2d");
+
+const saveLayer = async (_canvas) => {
+    fs.writeFileSync("./new-gunung-01.png", _canvas.toBuffer("image/png"));
+};
+
+const drawLayer = async () => {
+    const image = loadImage('./Gunung-01.png');
+    ctx.drawImage(image, 0, 0, 1000, 1000);
+    saveLayer(canvas);
+};
+
+drawLayer();
