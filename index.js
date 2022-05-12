8c25ae938c309e7bb761ed75a174d308bd3a6ea3
Kevin Dave <kevindavee1505@gmail.com>
2022-05-12 20:04:22 +0700

    await load image

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const saveLayer = async (_canvas) => {
 };
 
 const drawLayer = async () => {
-    const image = loadImage('./Gunung-01.png');
+    const image = await loadImage('./Gunung-01.png');
     ctx.drawImage(image, 0, 0, 1000, 1000);
     saveLayer(canvas);
 };
