dd6b168ca784fccc929bbae69ef7dbf40852923e
Kevin Dave <kevindavee1505@gmail.com>
2022-05-12 20:39:06 +0700

    add await

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const { createCanvas, loadImage } = require("canvas");
 const canvas = createCanvas(1000, 1000);
 const ctx = canvas.getContext("2d");
 const { layers } = require('./input/config');
-const edition = 1;
+const edition = 10;
 
 const saveLayer = async (_canvas, _edition) => {
     fs.writeFileSync(`./output/${_edition}.png`, _canvas.toBuffer("image/png"));
 };
 
 const drawLayer = async (_layer, _edition) => {
-    let element = _layer.elements[Math.floor(Math.random() * _layer.element.length)];
+    let element = _layer.elements[Math.floor(Math.random() * _layer.elements.length)];
     const image = await loadImage(`${_layer.location}${element.fileName}`);
     ctx.drawImage(image, _layer.position.x, _layer.position.y, _layer.size.width, _layer.size.height);
     saveLayer(canvas);
