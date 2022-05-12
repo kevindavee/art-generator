623ca39ff8d9bf191e742779bb5bd40dd7fd5619
Kevin Dave <kevindavee1505@gmail.com>
2022-05-12 21:20:03 +0700

    bug fix

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ const computeMetadata = (_edition) => {
         decodedHash: decodedHash,
     };
     hash = [];
+    attributes = [];
+    decodedHash = [];
     return tempMetadata;
 };
 
@@ -45,13 +47,16 @@ const drawLayer = async (_layer, _edition) => {
     addAttribute(element, _layer);
     const image = await loadImage(`${_layer.location}${element.fileName}`);
     ctx.drawImage(image, _layer.position.x, _layer.position.y, _layer.size.width, _layer.size.height);
-    saveLayer(canvas, _edition);
+    await saveLayer(canvas, _edition);
 };
 
-for (let i = 0; i < edition; i++) {
-    layers.forEach(layer => {
-        drawLayer(layer, i);
-    });
-    const metadata = computeMetadata(i);
-    fs.writeFileSync(`./output/metadata/${i}.json`, JSON.stringify(metadata, null, 2));
-}
+(async () => {
+    for (let i = 0; i < edition; i++) {
+        for (const layer of layers) {
+            await drawLayer(layer, i);
+        }
+        const metadata = computeMetadata(i);
+        fs.writeFileSync(`./output/metadata/${i}.json`, JSON.stringify(metadata, null, 2));
+        ctx.clearRect(0, 0, 1000, 1000);
+    }
+})();
